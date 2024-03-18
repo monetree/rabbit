d00d016105279e022339eacbf361e8fdce201b3d
soubhagya <soubhagya@potential.ai>
2024-03-18 14:09:02 +0530

    added utils

---
+++ src/utils/Constants.js
@@ -0,0 +1,6 @@
+export const API_URL = import.meta.env.VITE_API_URL;
+
+export const VITE_MARKETPLACE_URL = import.meta.env.VITE_MARKETPLACE_URL;
+export const VITE_DASHBOARD_URL = import.meta.env.VITE_DASHBOARD_URL;
+
+export const GOOGLE_LOGIN_API_KEY = import.meta.env.VITE_GOOGLE_LOGIN_API_KEY;
