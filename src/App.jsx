ce46ec6060642e6d7371b010138dd4d8fa23a820
soubhagya <soubhagya@potential.ai>
2024-03-18 11:21:54 +0530

    added landing

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,31 @@
-import './App.css';
-import HeroSection from './components/HeroSection';
-import Header from './components/Header';
-import HowItWorks from './components/HowItWorks';
-import OurPartners from './components/OurPartners';
-import WhatOurCustomerSays from './components/WhatOurCustomerSays';
-import Benefits from './components/Benefits';
-import Pricing from './components/Pricing';
-import ExploreByCat from './components/ExploreByCat';
-import Faqs from './components/Faqs';
-import Cta from './components/Cta';
-import Footer from './components/Footer';
-import 'bootstrap-icons/font/bootstrap-icons.css';
+import HeroSection from "./components/HeroSection";
+import Header from "./components/Header";
+import HowItWorks from "./components/HowItWorks";
+import OurPartners from "./components/OurPartners";
+import WhatOurCustomerSays from "./components/WhatOurCustomerSays";
+import Benefits from "./components/Benefits";
+import Pricing from "./components/Pricing";
+import ExploreByCat from "./components/ExploreByCat";
+import Faqs from "./components/Faqs";
+import Cta from "./components/Cta";
+import Footer from "./components/Footer";
+import "bootstrap-icons/font/bootstrap-icons.css";
+
 function App() {
   return (
-    <div className='pt-16 lg:pt-20'>
-      <Header />
+    <div className="pt-16 lg:pt-20">
+      {/* <Header /> */}
       <HeroSection />
       <OurPartners />
       <HowItWorks />
       <WhatOurCustomerSays />
 
-      <Benefits />
+      {/* <Benefits />
       <Pricing />
       <ExploreByCat />
       <Faqs />
       <Cta />
-      <Footer />
+      <Footer /> */}
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,122 +1,122 @@
-import React, { useState } from 'react';
-import Logo from '../assets/images/Logo.png';
+import React, { useState } from "react";
+import Logo from "../assets/images/Logo.png";
 
 const Header = () => {
   const [menu, setMenu] = useState(false);
   return (
-    <div class='fixed top-0 left-0 right-0 z-40  '>
+    <div class="fixed top-0 left-0 right-0 z-40  ">
       <div
         div
-        class=' relative bg-white px-3 lg:px-4 top-navbar border-b  border-selfGray/30 pt-1'
+        class=" relative bg-white px-3 lg:px-4 top-navbar border-b  border-selfGray/30 pt-1"
       >
-        <div class='container mx-auto my-auto '>
-          <div class='grid grid-cols-12 h-16 lg:h-20'>
-            <div class='col-span-12 lg:col-span-3 '>
-              <div class='flex justify-between items-center h-full mt-1'>
-                <div class='logo-div'>
-                  <a href='#'>
-                    <img src={Logo} alt='' class=' h-6' />
+        <div class="container mx-auto my-auto ">
+          <div class="grid grid-cols-12 h-16 lg:h-20">
+            <div class="col-span-12 lg:col-span-3 ">
+              <div class="flex justify-between items-center h-full mt-1">
+                <div class="logo-div">
+                  <a href="#">
+                    <img src={Logo} alt="" class=" h-6" />
                   </a>
                 </div>
 
                 <div
                   onClick={() => setMenu(true)}
-                  class='menu-btn block lg:hidden'
+                  class="menu-btn block lg:hidden"
                 >
-                  <i class='bi bi-list text-[26px]'></i>
+                  <i class="bi bi-list text-[26px]"></i>
                 </div>
               </div>
             </div>
-            <div class='col-span-9 flex items-center '>
+            <div class="col-span-9 flex items-center ">
               <div
                 class={`
               manu-div mb-5 lg:mb-0 transition-all duration-300  ${
-                menu ? 'scale-100' : 'scale-0'
+                menu ? "scale-100" : "scale-0"
               }  lg:scale-[1]  fixed top-0 left-0 right-0 bottom-0 lg:relative h-[100vh] lg:h-auto w-full   bg-darkGreen lg:bg-white flex justify-center items-center
               `}
               >
-                <div class='grid grid-cols-9 w-full '>
+                <div class="grid grid-cols-9 w-full ">
                   <div
                     onClick={() => setMenu(false)}
-                    class='close-btn block lg:hidden ms-auto absolute top-[30px] right-[30px]'
+                    class="close-btn block lg:hidden ms-auto absolute top-[30px] right-[30px]"
                   >
-                    <i class='bi bi-x-lg text-[20px] text-white'></i>
+                    <i class="bi bi-x-lg text-[20px] text-white"></i>
                   </div>
-                  <div class='col-span-9 lg:col-span-6 flex items-center justify-center '>
-                    <ul class='flex items-center justify-center  text-start flex-col lg:flex-row lg:mt-2  '>
+                  <div class="col-span-9 lg:col-span-6 flex items-center justify-center ">
+                    <ul class="flex items-center justify-center  text-start flex-col lg:flex-row lg:mt-2  ">
                       <li>
-                        <div class='block lg:hidden mb-4'>
-                          <a href='#'>
-                            <img src={Logo} alt='' class='h-[27px] w-auto' />
+                        <div class="block lg:hidden mb-4">
+                          <a href="#">
+                            <img src={Logo} alt="" class="h-[27px] w-auto" />
                           </a>
                         </div>
                       </li>
-                      <li class='lg:ms-[20px]  xl:ms-[25px]  mb-3 lg:mb-0 w-fit  border-b-2 border-transparent hover:border-coffee border-solid active-nav'>
+                      <li class="lg:ms-[20px]  xl:ms-[25px]  mb-3 lg:mb-0 w-fit  border-b-2 border-transparent hover:border-coffee border-solid active-nav">
                         <a
-                          href='#'
-                          class='lg:hover:text-coffee text-base font-medium leading-normal tracking-normal text-center text-white lg:text-darkGreen '
+                          href="#"
+                          class="lg:hover:text-coffee text-base font-medium leading-normal tracking-normal text-center text-white lg:text-darkGreen "
                         >
                           Home
                         </a>
                       </li>
-                      <li class='lg:ms-[20px]  xl:ms-[25px]  mb-3 lg:mb-0 w-fit  border-b-2 border-transparent hover:border-coffee border-solid'>
+                      <li class="lg:ms-[20px]  xl:ms-[25px]  mb-3 lg:mb-0 w-fit  border-b-2 border-transparent hover:border-coffee border-solid">
                         <a
-                          href='#howItWorks'
-                          class='lg:hover:text-coffee text-base font-medium leading-normal tracking-normal text-center text-white lg:text-darkGreen '
+                          href="#howItWorks"
+                          class="lg:hover:text-coffee text-base font-medium leading-normal tracking-normal text-center text-white lg:text-darkGreen "
                         >
                           How It Works
                         </a>
                       </li>
-                      <li class='lg:ms-[20px]  xl:ms-[25px]  mb-3 lg:mb-0 w-fit  border-b-2 border-transparent hover:border-coffee border-solid'>
+                      <li class="lg:ms-[20px]  xl:ms-[25px]  mb-3 lg:mb-0 w-fit  border-b-2 border-transparent hover:border-coffee border-solid">
                         <a
-                          href='#benefits'
-                          class='lg:hover:text-coffee text-base font-medium leading-normal tracking-normal text-center text-white lg:text-darkGreen '
+                          href="#benefits"
+                          class="lg:hover:text-coffee text-base font-medium leading-normal tracking-normal text-center text-white lg:text-darkGreen "
                         >
                           Benefits
                         </a>
                       </li>
                       <li
-                        id='faq-item'
-                        class='lg:ms-[20px]  xl:ms-[25px]  mb-3 lg:mb-0 w-fit  border-b-2 border-transparent hover:border-coffee border-solid'
+                        id="faq-item"
+                        class="lg:ms-[20px]  xl:ms-[25px]  mb-3 lg:mb-0 w-fit  border-b-2 border-transparent hover:border-coffee border-solid"
                       >
                         <a
-                          href='#faqs'
-                          class='lg:hover:text-coffee text-base font-medium leading-normal tracking-normal text-center text-white lg:text-darkGreen '
+                          href="#faqs"
+                          class="lg:hover:text-coffee text-base font-medium leading-normal tracking-normal text-center text-white lg:text-darkGreen "
                         >
                           FAQs
                         </a>
                       </li>
                       <li
-                        id='form-item'
-                        class='lg:ms-[20px]  xl:ms-[25px]  mb-3 lg:mb-0 w-fit  border-b-2 border-transparent hover:border-coffee border-solid'
+                        id="form-item"
+                        class="lg:ms-[20px]  xl:ms-[25px]  mb-3 lg:mb-0 w-fit  border-b-2 border-transparent hover:border-coffee border-solid"
                       >
                         <a
-                          href='#eligibleProducts'
-                          class='lg:hover:text-coffee text-base font-medium leading-normal tracking-normal text-center text-white lg:text-darkGreen '
+                          href="#eligibleProducts"
+                          class="lg:hover:text-coffee text-base font-medium leading-normal tracking-normal text-center text-white lg:text-darkGreen "
                         >
                           Eligible Products
                         </a>
                       </li>
                     </ul>
                   </div>
-                  <div class=' col-span-9 lg:col-span-3 flex flex-col lg:flex-row items-center justify-end '>
+                  <div class=" col-span-9 lg:col-span-3 flex flex-col lg:flex-row items-center justify-end ">
                     <div
-                      class=' text-darkGreen font-medium lg:text-white border-2 mb-4 lg:mb-0
+                      class=" text-darkGreen font-medium lg:text-white border-2 mb-4 lg:mb-0
                      hover:shadow-custom hover:border-2 border-darkGreen rounded-[30px] cursor-pointer
-                      text-center border-solid bg-white lg:bg-darkGreen hover:bg-white lg:hover:text-darkGreen w-32 h-10 flex align-middle justify-center items-center'
+                      text-center border-solid bg-white lg:bg-darkGreen hover:bg-white lg:hover:text-darkGreen w-32 h-10 flex align-middle justify-center items-center"
                     >
-                      <a href='#' class='leading-5 text-sm font-medium'>
+                      <a href="#" class="leading-5 text-sm font-medium">
                         Start Saving
                       </a>
                     </div>
 
-                    <div class='lg:ms-[10px]  xl:ms-[15px]  mb-3 lg:mb-0 w-fit '>
+                    <div class="lg:ms-[10px]  xl:ms-[15px]  mb-3 lg:mb-0 w-fit ">
                       <div
-                        class='  font-medium  border-2
+                        class="  font-medium  border-2
                      hover:shadow-custom  lg:border-darkGreen rounded-[30px] cursor-pointer
-                      text-center border-solid bg-transparent hover:bg-darkGreen lg:hover:text-white text-white lg:text-darkGreen w-32 h-10 flex align-middle justify-center items-center'
+                      text-center border-solid bg-transparent hover:bg-darkGreen lg:hover:text-white text-white lg:text-darkGreen w-32 h-10 flex align-middle justify-center items-center"
                       >
-                        <a href='#' class='leading-5 text-sm font-medium'>
+                        <a href="#" class="leading-5 text-sm font-medium">
                           Log In
                         </a>
                       </div>
