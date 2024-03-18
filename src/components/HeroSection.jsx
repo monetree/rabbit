93f533473e5b9b42c9f2c27a2984853d342689ff
soubhagya <soubhagya@potential.ai>
2024-03-18 14:49:20 +0530

    added pricing url

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,5 +1,7 @@
-import React from 'react';
-import Bg from '../assets/images/Main Block.png';
+import React from "react";
+import Bg from "../assets/images/Main Block.png";
+import { Link } from "react-router-dom";
+import { VITE_MARKETPLACE_URL } from "../utils/Constants";
 
 const HeroSection = () => {
   return (
@@ -9,24 +11,27 @@ const HeroSection = () => {
         backgroundImage: `url('${Bg}')`,
       }}
     >
-      <div className='container'>
-        <div className='flex'>
-          <div className='mx-auto lg:basis-9/12 md:basis-8/12 '>
-            <h1 className='text-4xl font-bold leading-tight tracking-normal text-center text-white font-epilogue md:text-4xl lg:text-5xl'>
+      <div className="container">
+        <div className="flex">
+          <div className="mx-auto lg:basis-9/12 md:basis-8/12 ">
+            <h1 className="text-4xl font-bold leading-tight tracking-normal text-center text-white font-epilogue md:text-4xl lg:text-5xl">
               Your Favourite Wellness Products 30% Cheaper
             </h1>
-            <p className='mt-4 text-base font-medium leading-7 tracking-normal text-center text-white font-epilogue'>
+            <p className="mt-4 text-base font-medium leading-7 tracking-normal text-center text-white font-epilogue">
               Explore a vast array of high-quality pharmaceuticals, supplements
               and healthcare products, Explore a vast array of high-quality
               pharmaceuticals, supplements and healthcare products,
             </p>
             <div
-              class=' mt-6 mx-auto bg-beige  border-2 hover:shadow-custom  rounded-[30px] cursor-pointer border-transparent
-        text-center border-solid   text-darkGreen   h-12 flex align-middle justify-center items-center w-56 hover:bg-darkGreen hover:text-white'
+              class=" mt-6 mx-auto bg-beige  border-2 hover:shadow-custom  rounded-[30px] cursor-pointer border-transparent
+        text-center border-solid   text-darkGreen   h-12 flex align-middle justify-center items-center w-56 hover:bg-darkGreen hover:text-white"
             >
-              <a href='' class='leading-5 text-base font-semibold'>
+              <Link
+                to={`${VITE_MARKETPLACE_URL}`}
+                class="leading-5 text-base font-semibold"
+              >
                 Start Saving
-              </a>
+              </Link>
             </div>
           </div>
         </div>
