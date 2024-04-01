fc21191813e9e41834b07a4c1689755313418bd6
soubhagya <soubhagya@potential.ai>
2024-04-01 16:45:53 +0530

    added landing

--- src/components/Faqs.jsx
+++ src/components/Faqs.jsx
@@ -4,7 +4,7 @@ const Faqs = () => {
   return (
     <section
       id="faqs"
-      class="px-4 xl:px-4  bg-cover bg-bottom  pb-14 pt-12 bg-pitch"
+      class="px-4 xl:px-4  bg-cover bg-beige bg-bottom  pb-14 pt-12 bg-pitch"
     >
       <div class="container mx-auto">
         <div class="mx-auto ">
@@ -59,14 +59,13 @@ const Faqs = () => {
                   <Accordion.Content className="!bg-white relative !rounded-br-xl !rounded-bl-xl -mt-2 after:absolute after:top-2 after:left-5 after:right-5 after:opacity-40 after:border-b after:border-selfGray">
                     <div class="py-5   border-gray-200 dark:border-gray-700">
                       <p class="mb-0 pb-0 text-sm text-selfGray ">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Ratione et iusto porro facere quidem excepturi quaerat
-                        quis nobis delectus nemo quae, cumque eaque ipsam
-                        reiciendis sapiente! Et molestias aspernatur minus.
-                        adipisicing elit. Ratione et iusto porro facere quidem
-                        excepturi quaerat quis nobis delectus nemo quae, cumque
-                        eaque ipsam reiciendis sapiente! Et molestias aspernatur
-                        minus.
+                        Crates enables you to use HSA/FSA dollars. By using your
+                        HSA/FSA dollars, you are using pre-tax dollars to
+                        purchase products or services. This means that you do
+                        not pay state or federal taxes on those dollars spent.
+                        On average, that is 30% savings. For some of our users,
+                        the savings are thousands of dollars. On average, we
+                        save users $325 annually
                       </p>
                     </div>
                   </Accordion.Content>
@@ -79,14 +78,14 @@ const Faqs = () => {
                   <Accordion.Content className="!bg-white relative !rounded-br-xl !rounded-bl-xl -mt-2 after:absolute after:top-2 after:left-5 after:right-5 after:opacity-40 after:border-b after:border-selfGray">
                     <div class="py-5   border-gray-200 dark:border-gray-700">
                       <p class="mb-0 pb-0 text-sm text-selfGray ">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Ratione et iusto porro facere quidem excepturi quaerat
-                        quis nobis delectus nemo quae, cumque eaque ipsam
-                        reiciendis sapiente! Et molestias aspernatur minus.
-                        adipisicing elit. Ratione et iusto porro facere quidem
-                        excepturi quaerat quis nobis delectus nemo quae, cumque
-                        eaque ipsam reiciendis sapiente! Et molestias aspernatur
-                        minus.
+                        Once you've purchased your product, you upload a photo
+                        or PDF of your receipt. We will then initiate the
+                        reimbursement process with our partner HSA/FSA
+                        administrators. The process can take anywhere from 4-21
+                        days depending on your HSA administrator. We are always
+                        working closely with administrators to bring this time
+                        down. Of course, you always have the option to file for
+                        reimbursement directly with your HSA/FSA administrator.
                       </p>
                     </div>
                   </Accordion.Content>
@@ -99,14 +98,10 @@ const Faqs = () => {
                   <Accordion.Content className="!bg-white relative !rounded-br-xl !rounded-bl-xl -mt-2 after:absolute after:top-2 after:left-5 after:right-5 after:opacity-40 after:border-b after:border-selfGray">
                     <div class="py-5   border-gray-200 dark:border-gray-700">
                       <p class="mb-0 pb-0 text-sm text-selfGray ">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Ratione et iusto porro facere quidem excepturi quaerat
-                        quis nobis delectus nemo quae, cumque eaque ipsam
-                        reiciendis sapiente! Et molestias aspernatur minus.
-                        adipisicing elit. Ratione et iusto porro facere quidem
-                        excepturi quaerat quis nobis delectus nemo quae, cumque
-                        eaque ipsam reiciendis sapiente! Et molestias aspernatur
-                        minus.
+                        A Letter of Medical Necessity (LMN) is a formal document
+                        that explains why a patient needs a specific treatment,
+                        product, or service. A licensed healthcare provider
+                        writes the document.
                       </p>
                     </div>
                   </Accordion.Content>
@@ -120,14 +115,17 @@ const Faqs = () => {
                   <Accordion.Content className="!bg-white relative !rounded-br-xl !rounded-bl-xl -mt-2 after:absolute after:top-2 after:left-5 after:right-5 after:opacity-40 after:border-b after:border-selfGray">
                     <div class="py-5   border-gray-200 dark:border-gray-700">
                       <p class="mb-0 pb-0 text-sm text-selfGray ">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Ratione et iusto porro facere quidem excepturi quaerat
-                        quis nobis delectus nemo quae, cumque eaque ipsam
-                        reiciendis sapiente! Et molestias aspernatur minus.
-                        adipisicing elit. Ratione et iusto porro facere quidem
-                        excepturi quaerat quis nobis delectus nemo quae, cumque
-                        eaque ipsam reiciendis sapiente! Et molestias aspernatur
-                        minus.
+                        Absolutely! We are always evaluating new categories,
+                        brands, and products. Please send us an email at
+                        <a
+                          href="mailto:info@ourservice.com"
+                          className=" text-beige hover:text-darkGreen"
+                        >
+                          info@crateshealth.com
+                        </a>{" "}
+                        with your suggestion. If you are Ultimate Saver
+                        subscriber, you have the option to submit a custom
+                        request and we will prioritize these requests.
                       </p>
                     </div>
                   </Accordion.Content>
