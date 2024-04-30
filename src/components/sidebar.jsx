7253223893869c663a6cb85554e879f44eb44163
soubhagya <soubhagya@potential.ai>
2024-04-30 11:33:23 +0530

    added api

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 const Sidebar = ({ setTab }) => {
   return (
@@ -10,10 +10,13 @@ const Sidebar = ({ setTab }) => {
       <div className="h-full px-3 pb-4 overflow-y-auto bg-white ">
         <ul className="space-y-2 font-medium">
           <li>
-            <Link
+            <NavLink
               to="/dashboard/hsa-administrators"
               onClick={() => setTab("hsa-administrators")}
-              className="flex cursor-pointer items-center p-2 text-gray-900 rounded-lg  hover:bg-gray-100  group"
+              className={({ isActive }) =>
+                "flex cursor-pointer items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group" +
+                (isActive ? " bg-gray-100" : "")
+              }
             >
               <svg
                 className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75  group-hover:text-gray-900"
@@ -27,13 +30,16 @@ const Sidebar = ({ setTab }) => {
               <span className="flex-1 ms-3 whitespace-nowrap">
                 Hsa Administrators
               </span>
-            </Link>
+            </NavLink>
           </li>
           <li>
-            <Link
+            <NavLink
               to="/dashboard/health-conditions"
               onClick={() => setTab("health-conditions")}
-              className="flex items-center cursor-pointer p-2 text-gray-900 rounded-lg  hover:bg-gray-100  group"
+              className={({ isActive }) =>
+                "flex items-center cursor-pointer p-2 text-gray-900 rounded-lg hover:bg-gray-100 group" +
+                (isActive ? " bg-gray-100" : "")
+              }
             >
               <svg
                 className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75  group-hover:text-gray-900 "
@@ -47,13 +53,16 @@ const Sidebar = ({ setTab }) => {
               <span className="flex-1 ms-3 whitespace-nowrap">
                 Health Conditions
               </span>
-            </Link>
+            </NavLink>
           </li>
           <li>
-            <Link
+            <NavLink
               to="/dashboard/categories"
               onClick={() => setTab("categories")}
-              className="flex items-center p-2 cursor-pointer text-gray-900 rounded-lg  hover:bg-gray-100  group"
+              className={({ isActive }) =>
+                "flex items-center p-2 cursor-pointer text-gray-900 rounded-lg hover:bg-gray-100 group" +
+                (isActive ? " bg-gray-100" : "")
+              }
             >
               <svg
                 className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75  group-hover:text-gray-900 "
@@ -65,13 +74,16 @@ const Sidebar = ({ setTab }) => {
                 <path d="M14 2a3.963 3.963 0 0 0-1.4.267 6.439 6.439 0 0 1-1.331 6.638A4 4 0 1 0 14 2Zm1 9h-1.264A6.957 6.957 0 0 1 15 15v2a2.97 2.97 0 0 1-.184 1H19a1 1 0 0 0 1-1v-1a5.006 5.006 0 0 0-5-5ZM6.5 9a4.5 4.5 0 1 0 0-9 4.5 4.5 0 0 0 0 9ZM8 10H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5Z" />
               </svg>
               <span className="flex-1 ms-3 whitespace-nowrap">Categories</span>
-            </Link>
+            </NavLink>
           </li>
           <li>
-            <Link
+            <NavLink
               to="/dashboard/sub-categories"
               onClick={() => setTab("sub-categories")}
-              className="flex items-center p-2 cursor-pointer text-gray-900 rounded-lg  hover:bg-gray-100 group"
+              className={({ isActive }) =>
+                "flex items-center p-2 cursor-pointer text-gray-900 rounded-lg hover:bg-gray-100 group" +
+                (isActive ? " bg-gray-100" : "")
+              }
             >
               <svg
                 className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75  group-hover:text-gray-900 "
@@ -85,14 +97,17 @@ const Sidebar = ({ setTab }) => {
               <span className="flex-1 ms-3 whitespace-nowrap">
                 Sub Categories
               </span>
-            </Link>
+            </NavLink>
           </li>
 
           <li>
-            <Link
+            <NavLink
               to="/dashboard/brands"
               onClick={() => setTab("brands")}
-              className="flex items-center p-2 cursor-pointer text-gray-900 rounded-lg  hover:bg-gray-100 group"
+              className={({ isActive }) =>
+                "flex items-center p-2 cursor-pointer text-gray-900 rounded-lg hover:bg-gray-100 group" +
+                (isActive ? " bg-gray-100" : "")
+              }
             >
               <svg
                 className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75  group-hover:text-gray-900 "
@@ -104,7 +119,7 @@ const Sidebar = ({ setTab }) => {
                 <path d="M14 2a3.963 3.963 0 0 0-1.4.267 6.439 6.439 0 0 1-1.331 6.638A4 4 0 1 0 14 2Zm1 9h-1.264A6.957 6.957 0 0 1 15 15v2a2.97 2.97 0 0 1-.184 1H19a1 1 0 0 0 1-1v-1a5.006 5.006 0 0 0-5-5ZM6.5 9a4.5 4.5 0 1 0 0-9 4.5 4.5 0 0 0 0 9ZM8 10H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5Z" />
               </svg>
               <span className="flex-1 ms-3 whitespace-nowrap">Brands</span>
-            </Link>
+            </NavLink>
           </li>
         </ul>
       </div>
