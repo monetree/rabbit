4800067c562f5ec86ae86a3404910ed36318322b
soubhagya <soubhagya@potential.ai>
2024-05-06 05:42:40 +0530

    added summary

--- src/components/summary.jsx
+++ src/components/summary.jsx
@@ -178,6 +178,20 @@ const Summary = () => {
                   >
                     Create LMN
                   </button>
+
+                  {item.file_location ? (
+                    <a
+                      href={item.file_location}
+                      target="_blank"
+                      onClick={() => setBrand(item)}
+                      type="button"
+                      class="px-3 py-2 ml-2 text-xs font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                    >
+                      View LMN
+                    </a>
+                  ) : (
+                    ""
+                  )}
                 </td>
               </tr>
             ))}
