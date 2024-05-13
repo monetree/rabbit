10c4a5980ca19998321cd32a4b53917d7a489858
soubhagya <soubhagya@potential.ai>
2024-05-13 19:30:03 +0530

    added changes

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -29,7 +29,7 @@ router
     }
   );
 
-router.route("/users").get(authenticateToken, function (req, res) {
+router.route("/users").get(function (req, res) {
   const result = fetchUsers(req, res);
   return result;
 });
@@ -37,15 +37,10 @@ router.route("/users").get(authenticateToken, function (req, res) {
 router
   .route("/create-user")
   .post(
-    authenticateToken,
     [
       check("name").notEmpty().withMessage("Name is required"),
       check("email").isEmail().withMessage("Email is invalid"),
-      check("phone")
-        .notEmpty()
-        .withMessage("Phone is required")
-        .isMobilePhone()
-        .withMessage("Phone number is invalid"),
+      check("phone").notEmpty().withMessage("Phone is required"),
     ],
     function (req, res) {
       // Check for validation errors
@@ -62,16 +57,10 @@ router
 router
   .route("/update-user")
   .patch(
-    authenticateToken,
     [
       check("name").optional().notEmpty().withMessage("Name cannot be empty"),
       check("email").optional().isEmail().withMessage("Email is invalid"),
-      check("phone")
-        .optional()
-        .notEmpty()
-        .withMessage("Phone cannot be empty")
-        .isMobilePhone()
-        .withMessage("Phone number is invalid"),
+      check("phone").optional().notEmpty().withMessage("Phone cannot be empty"),
     ],
     function (req, res) {
       const errors = validationResult(req);
@@ -84,7 +73,7 @@ router
     }
   );
 
-router.route("/delete-user/:id").delete(authenticateToken, function (req, res) {
+router.route("/delete-user/:id").delete(function (req, res) {
   const result = userDelete(req, res);
   return result;
 });
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,13 @@
 import "bootstrap-icons/font/bootstrap-icons.css";
-import { Route, Routes, BrowserRouter } from "react-router-dom";
+import { Route, Routes, BrowserRouter, Navigate } from "react-router-dom";
 import Dashboard from "./views/dashboard";
-import Login from "./views/login";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Navigate to="/dashboard" />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
